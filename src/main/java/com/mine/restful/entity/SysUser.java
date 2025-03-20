abb18aac399b324fad17c43e73a7980ee3927741
zhanghanwen <zhanghanwen@focoon.com.cn>
2025-03-20 17:53:25 +0800

    搭建框架

--- src/main/java/com/mine/restful/entity/SysUser.java
+++ src/main/java/com/mine/restful/entity/SysUser.java
@@ -71,7 +71,7 @@ public class SysUser implements Serializable {
     * 修改时间
     */
     @ApiModelProperty("修改时间")
-    private Date modifiedTime;
+    private String modifiedTime;
     /**
     * 创建用户
     */
